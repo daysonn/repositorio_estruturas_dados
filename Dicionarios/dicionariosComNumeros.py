d8b01228b70f65e45e131f5f46dcc89a8a92815c
Dayson Nascimento <dayson.ncrn@gmail.com>
2022-07-07 21:15:45 -0300

    Adicionando arquivos da aula de dicionários do dia 07-07-22

---
+++ Dicionarios/dicionariosComNumeros.py
@@ -0,0 +1,13 @@
+
+# Declaração de dicionário com chaves em inglês e valor numérico
+inglesParaNumeros = {'one': 1, 'two': 2, 'three': 3}
+
+# Declaração de dicionário com chaves numéricas e valor em inglês
+numerosParaIngles = dict()
+numerosParaIngles[1] = 'one'
+numerosParaIngles[2] = 'two'
+numerosParaIngles[3] = 'three'
+
+print(inglesParaNumeros)
+print(numerosParaIngles)
+
